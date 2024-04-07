b4ce3b90bea6c2dd738ae4e9404faf2d9105601c
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2024-04-07 16:33:16 +0700

    test algorithm by using or-tools of gg dev

---
+++ ortool/src/test/java/com/example/ortool/OrtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ortool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
