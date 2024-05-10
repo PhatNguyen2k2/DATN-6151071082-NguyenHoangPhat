991d2981e0bfd460ef87a1b6899e2bcc7a233b6e
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2024-05-10 20:56:00 +0700

    remove comment

--- CoordinateTransportationResources/src/main/java/com/phat/ctrs/service/impl/RouteServiceImpl.java
+++ CoordinateTransportationResources/src/main/java/com/phat/ctrs/service/impl/RouteServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,105 +49,6 @@ public class RouteServiceImpl implements IRouteService {
 	}
 
 	@Override
-	// public void calculateCost() {
-	// List<List<Route>> routeList = getRouteByVehicleType();
-	// routeList.forEach(route -> {
-	// BigDecimal vehicleTypeId = route.get(0).getVehicleType().getVehicleTypeId();
-	// List<Partner> partnerList =
-	// partnerService.getPartnersByVehicleType(vehicleTypeId);
-	// List<Integer> routeLength = new ArrayList<Integer>();
-	// List<Integer> costPartner = new ArrayList<Integer>();
-	// List<Integer> provideAbility = new ArrayList<Integer>();
-	// List<VehicleLoadType> vehicleLoadTypes = new ArrayList<VehicleLoadType>();
-	// List<List<LocalTime[]>> serveTime = new ArrayList<List<LocalTime[]>>();
-	// route.forEach(t ->
-	// routeLength.add(Integer.valueOf(t.getRouteLength().intValue())));
-	// partnerList.forEach(t -> {
-	// costPartner.add(Integer.valueOf(
-	// partnerService.getFeeOnKmOfPartnerByVehicleType(t.getPartnerId(),
-	// vehicleTypeId).intValue()));
-	// provideAbility.add(Integer
-	// .valueOf(partnerService.getResourceOfPartner(t.getPartnerId(),
-	// vehicleTypeId).intValue()));
-	// vehicleLoadTypes.add(partnerService.getVehicleLoadTypesByPartnerId(t.getPartnerId(),
-	// vehicleTypeId));
-	// });
-
-	// vehicleLoadTypes.forEach(t -> {
-	// LocalTime[] time = { t.getShift().getTimeStart(), t.getShift().getTimeEnd()
-	// };
-	// List<LocalTime[]> times = OrToolsHelper.calculateOuTimes(time);
-	// serveTime.add(times);
-	// });
-	// int costs[][] = OrToolsHelper.buildCostMatrix(routeLength, costPartner);
-	// int totalSizeMax[] = OrToolsHelper.buildProvideAbility(provideAbility);
-	// int times[][] = OrToolsHelper.buildShiftOfRoute(route);
-	// int shiftTimes[][][] = OrToolsHelper.buildServeTimeOfPartner(serveTime);
-	// HashMap<Integer, List<Integer>> rs = OrToolForBookingPartner.or(costs, times,
-	// shiftTimes, totalSizeMax);
-
-	// Set<Integer> keys = rs.keySet();
-	// for (int key : keys) {
-	// Partner pn = partnerList.get(key);
-	// List<Integer> routeRs = rs.get(key);
-	// for (int _route : routeRs) {
-	// Route r = route.get(_route);
-	// planService.updatePartnerForRoute(pn.getPartnerId(), r.getRouteId());
-	// }
-	// }
-	// });
-	// routeList.parallelStream()
-	// .map(route -> {
-	// BigDecimal vehicleTypeId = route.get(0).getVehicleType().getVehicleTypeId();
-	// List<Partner> partnerList =
-	// partnerService.getPartnersByVehicleType(vehicleTypeId);
-	// List<Integer> routeLength = route.stream().mapToInt(t ->
-	// t.getRouteLength().intValue()).boxed()
-	// .collect(Collectors.toList());
-
-	// List<Integer> costPartner = partnerList.stream()
-	// .map(t -> partnerService.getFeeOnKmOfPartnerByVehicleType(t.getPartnerId(),
-	// vehicleTypeId)
-	// .intValue())
-	// .collect(Collectors.toList());
-
-	// List<Integer> provideAbility = partnerList.stream()
-	// .map(t -> partnerService.getResourceOfPartner(t.getPartnerId(),
-	// vehicleTypeId)
-	// .intValue())
-	// .collect(Collectors.toList());
-
-	// List<VehicleLoadType> vehicleLoadTypes = partnerList.stream()
-	// .map(t -> partnerService.getVehicleLoadTypesByPartnerId(t.getPartnerId(),
-	// vehicleTypeId))
-	// .collect(Collectors.toList());
-
-	// List<List<LocalTime[]>> serveTime = vehicleLoadTypes.stream()
-	// .map(t -> OrToolsHelper.calculateOuTimes(
-	// new LocalTime[] { t.getShift().getTimeStart(), t.getShift().getTimeEnd() }))
-	// .collect(Collectors.toList());
-
-	// int[][] costs = OrToolsHelper.buildCostMatrix(routeLength, costPartner);
-	// int[] totalSizeMax = OrToolsHelper.buildProvideAbility(provideAbility);
-	// int[][] times = OrToolsHelper.buildShiftOfRoute(route);
-	// int[][][] shiftTimes = OrToolsHelper.buildServeTimeOfPartner(serveTime);
-
-	// HashMap<Integer, List<Integer>> rs = OrToolForBookingPartner.or(costs, times,
-	// shiftTimes,
-	// totalSizeMax);
-
-	// Set<Integer> keys = rs.keySet();
-	// for (int key : keys) {
-	// Partner pn = partnerList.get(key);
-	// List<Integer> routeRs = rs.get(key);
-	// for (int _route : routeRs) {
-	// Route r = route.get(_route);
-	// planService.updatePartnerForRoute(pn.getPartnerId(), r.getRouteId());
-	// }
-	// }
-	// });
-	// }
-
 	public void calculateCost() {
 		getRouteByVehicleType().parallelStream()
 				.forEach(route -> {
